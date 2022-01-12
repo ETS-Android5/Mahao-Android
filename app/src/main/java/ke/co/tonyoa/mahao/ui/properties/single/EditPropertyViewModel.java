91eb440a8a6e7ae3d73aaa73bf9d00988072fac1
tonyawino <ynotomondi57@gmail.com>
2022-01-12 18:36:56 +0300

    Add features for selecting location

--- app/src/main/java/ke/co/tonyoa/mahao/ui/properties/single/EditPropertyViewModel.java
+++ app/src/main/java/ke/co/tonyoa/mahao/ui/properties/single/EditPropertyViewModel.java
@@ -8,12 +8,16 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.Arrays;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import ke.co.tonyoa.mahao.app.MahaoApplication;
 import ke.co.tonyoa.mahao.app.api.APIResponse;
+import ke.co.tonyoa.mahao.app.api.responses.Property;
 import ke.co.tonyoa.mahao.app.api.responses.PropertyCategory;
 import ke.co.tonyoa.mahao.app.repositories.PropertyCategoriesRepository;
 
@@ -21,12 +25,14 @@ public class EditPropertyViewModel extends AndroidViewModel {
 
     private MutableLiveData<Uri> mThumbnailUri = new MutableLiveData<>();
     private MutableLiveData<PropertyCategory> mSelectedPropertyCategory = new MutableLiveData<>();
+    private MutableLiveData<Property> mPropertyMutableLiveData = new MutableLiveData<>();
     @Inject
     PropertyCategoriesRepository mPropertyCategoriesRepository;
 
-    public EditPropertyViewModel(@NonNull Application application) {
+    public EditPropertyViewModel(@NonNull Application application, Property property) {
         super(application);
         MahaoApplication.getInstance(application).getApplicationComponent().inject(this);
+        mPropertyMutableLiveData.postValue(property);
     }
 
     public void setThumbnailUri(Uri uri){
@@ -47,5 +53,22 @@ public class EditPropertyViewModel extends AndroidViewModel {
 
     public LiveData<PropertyCategory> getSelectedPropertyCategory(){
         return mSelectedPropertyCategory;
+    }
+
+    public LiveData<Property> getProperty() {
+        return mPropertyMutableLiveData;
+    }
+
+    public void setProperty(Property property){
+        mPropertyMutableLiveData.postValue(property);
+    }
+
+    public void setLocation(String location, LatLng coordinates){
+        Property property = mPropertyMutableLiveData.getValue();
+        if (property==null)
+            property = new Property();
+        property.setLocationName(location);
+        property.setLocation(Arrays.asList((float)coordinates.latitude, (float)coordinates.longitude));
+        mPropertyMutableLiveData.postValue(property);
     }
 }
--- app/src/main/java/ke/co/tonyoa/mahao/ui/properties/single/PickLocationFragment.java
+++ app/src/main/java/ke/co/tonyoa/mahao/ui/properties/single/PickLocationFragment.java
@@ -1,67 +1,263 @@
 package ke.co.tonyoa.mahao.ui.properties.single;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static ke.co.tonyoa.mahao.ui.home.HomeViewModel.DEFAULT_COORDINATES;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.Task;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.model.RectangularBounds;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 import ke.co.tonyoa.mahao.R;
+import ke.co.tonyoa.mahao.app.navigation.BaseFragment;
+import ke.co.tonyoa.mahao.app.utils.ViewUtils;
+import ke.co.tonyoa.mahao.databinding.FragmentPickLocationBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PickLocationFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
-public class PickLocationFragment extends Fragment {
+public class PickLocationFragment extends BaseFragment implements OnMapReadyCallback{
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final String PICK_LOCATION_REQUEST = "PICK_LOCATION_REQUEST";
+    public static final String CURRENT_LOCATION_EXTRA = "CURRENT_LOCATION";
+    public static final String LOCATION_NAME_EXTRA = "LOCATION_NAME";
+    public static final String LOCATION_LAT_LNG_EXTRA = "LOCATION_LAT_LNG";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentPickLocationBinding mFragmentPickLocationBinding;
+    private GoogleMap mGoogleMap;
+    private PlacesClient mPlacesClient;
+    private ActivityResultLauncher<String> mPermissionResultLauncher;
+    private ActivityResultLauncher<Intent> mLocationEnableLauncher;
+    private String mLocationName;
+    private float[] mCoordinates;
+    private boolean mClicked = true;
+    private OnLocationSelectedListener mOnLocationSelectedListener;
 
     public PickLocationFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PickLocationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PickLocationFragment newInstance(String param1, String param2) {
-        PickLocationFragment fragment = new PickLocationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mLocationName = PickLocationFragmentArgs.fromBundle(getArguments()).getName();
+            mCoordinates = PickLocationFragmentArgs.fromBundle(getArguments()).getCoordinates();
+            mOnLocationSelectedListener = PickLocationFragmentArgs.fromBundle(getArguments()).getListener();
         }
+        mPermissionResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), result -> {
+            if (result!=null && result){
+                setupCurrentLocation();
+            }
+            else {
+                Snackbar.make(mFragmentPickLocationBinding.linearLayoutLocationPickerFooter,
+                        "Grant Location Permission for correct functionality",
+                        BaseTransientBottomBar.LENGTH_INDEFINITE).setAction("Grant", v -> {
+                            mPermissionResultLauncher.launch(ACCESS_FINE_LOCATION);
+                        }).show();
+            }
+        });
+        mLocationEnableLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+           setupCurrentLocation();
+        });
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pick_location, container, false);
+        mFragmentPickLocationBinding = FragmentPickLocationBinding.inflate(inflater, container, false);
+        setToolbar(mFragmentPickLocationBinding.layoutToolbar.materialToolbarLayoutToolbar);
+        setTitle("Pick Location");
+
+        // Initialize the Places SDK
+        Places.initialize(requireContext(), getString(R.string.google_maps_key));
+
+        // Create a new PlacesClient instance
+        mPlacesClient = Places.createClient(requireContext());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.locationPicker_maps);
+        if (mapFragment!=null) {
+            mapFragment.getMapAsync(this);
+        }
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getChildFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        if (autocompleteFragment != null) {
+            // Specify the types of place data to return.
+            autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,
+                    Place.Field.LAT_LNG, Place.Field.ADDRESS));
+            autocompleteFragment.setLocationBias(RectangularBounds.newInstance(new LatLng( -4.893646, 33.896000 ),
+                    new LatLng( 4.543865, 41.916020 ))).setCountries("KE");
+
+
+            // Set up a PlaceSelectionListener to handle the response.
+            autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                @Override
+                public void onPlaceSelected(@NonNull Place place) {
+                    mClicked = true;
+                    mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 12));
+                    mFragmentPickLocationBinding.textViewLocationPickerCurrentAddress.setText(place.getName()+", "+place.getAddress());
+                }
+
+
+                @Override
+                public void onError(@NonNull Status status) {
+                    Log.i("PICK_LOCATION", "An error occurred: " + status);
+                }
+            });
+        }
+
+        mFragmentPickLocationBinding.locationPickerDestinationButton.setOnClickListener(v->{
+            LatLng selectedLocation = mGoogleMap.getCameraPosition().target;
+            String selectedAddress = ViewUtils.getText(mFragmentPickLocationBinding.textViewLocationPickerCurrentAddress);
+            Bundle bundle = new Bundle();
+            bundle.putString(LOCATION_NAME_EXTRA, selectedAddress);
+            bundle.putDoubleArray(LOCATION_LAT_LNG_EXTRA, new double[]{selectedLocation.latitude, selectedLocation.longitude});
+            if (mOnLocationSelectedListener!=null) {
+                mOnLocationSelectedListener.onLocationSelected(selectedAddress, selectedLocation);
+            }
+            navigateUp();
+        });
+
+        return mFragmentPickLocationBinding.getRoot();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        LatLng latlng = new LatLng(mCoordinates==null?DEFAULT_COORDINATES.latitude:mCoordinates[0],
+                mCoordinates==null?DEFAULT_COORDINATES.longitude:mCoordinates[1]);
+        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 12));
+        setupCurrentLocation();
+
+        mGoogleMap.setOnCameraIdleListener(() -> {
+            LatLng latLng = mGoogleMap.getCameraPosition().target;
+            // Only get place data if not set from search or current position
+            if (!mClicked) {
+                new Thread(() -> {
+                    try {
+                        Geocoder geocoder = new Geocoder(requireContext(), Locale.getDefault());
+                        final List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                        requireActivity().runOnUiThread(()->{
+                            if (!addresses.isEmpty()) {
+                                String address = addresses.get(0).getAddressLine(0);
+                                mFragmentPickLocationBinding.textViewLocationPickerCurrentAddress.setText(address);
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }
+            else {
+                mClicked = false;
+            }
+        });
+    }
+
+    private void setupCurrentLocation(){
+        // Use fields to define the data types to return.
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG);
+
+        // Use the builder to create a FindCurrentPlaceRequest.
+        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.newInstance(placeFields);
+
+        // Call findCurrentPlace and handle the response (first check that the user has granted permission).
+        if (ContextCompat.checkSelfPermission(requireContext(), ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (!ViewUtils.isLocationEnabled(requireContext())){
+                Snackbar.make(mFragmentPickLocationBinding.linearLayoutLocationPickerFooter,
+                        R.string.enable_your_location, Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.enable, v -> {
+                            Intent intent=new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            mLocationEnableLauncher.launch(intent);
+                        })
+                        .show();
+                return;
+            }
+
+            mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            mGoogleMap.setMyLocationEnabled(true);
+
+            if (mCoordinates==null) {
+                Task<FindCurrentPlaceResponse> placeResponse = mPlacesClient.findCurrentPlace(request);
+                placeResponse.addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        FindCurrentPlaceResponse response = task.getResult();
+                        List<PlaceLikelihood> placeLikelihoods = response.getPlaceLikelihoods();
+                        if (placeLikelihoods.size() > 0) {
+                            Place place = placeLikelihoods.get(0).getPlace();
+                            mClicked = true;
+                            mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.getLatLng(), 12));
+                            mFragmentPickLocationBinding.textViewLocationPickerCurrentAddress.setText(place.getName() + ", " + place.getAddress());
+                        }
+                    } else {
+                        Exception exception = task.getException();
+                        if (exception instanceof ApiException) {
+                            ApiException apiException = (ApiException) exception;
+                            Log.e("PICK_LOCATION", "Place not found: " + apiException.getStatusCode());
+                        }
+                    }
+                });
+            }
+        }
+        else {
+            mPermissionResultLauncher.launch(ACCESS_FINE_LOCATION);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == android.R.id.home) {
+            navigateUp();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    interface OnLocationSelectedListener extends Serializable {
+        void onLocationSelected(String locationName, LatLng latLng);
     }
 }
